7091dcf25f7c19b3014e98d73f0c0de20c73442a
titoiriepal <titoiriepal@hotmail.com>
2024-04-01 16:06:01 +0200

    Admin/crear.php form added

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -8,6 +8,56 @@
         <h1>Crer Propiedad</h1>
 
         <a href="javascript:history.back()" class="boton boton-verde">Volver</a>
+
+        <form class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Título:</label>
+                <input type="text" id="titulo" placeholder="Título de la propiedad" >
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio de la propiedad" >
+
+                <label for="imagen">Imágen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png, image/webp">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"> </textarea>
+
+                <fieldset>
+                    <legend>Información de la propiedad</legend>
+
+                    <label for="habitaciones">Número de Habitaciones:</label>
+                    <input type="number" id="habitaciones" placeholder="Número de habitaciones" min="1" max="9">
+
+                    <label for="wc">Número de baños:</label>
+                    <input type="number" id="wc" placeholder="Número de baños" min="1" max="9">
+
+                    <label for="estacionamiento">Número de plazas de parking:</label>
+                    <input type="number" id="estacionamiento" placeholder="Número de plazas de parking" min="1" max="9">
+                </fieldset>
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="vendedores_id" id="vendedores_id">
+
+                    <option disabled selected>Selecciona un vendedor</option>
+                    <option value="1">Antonio López</option>
+                    <option value="2">María Perez</option>
+                </select>
+                
+            
+
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+
+
+        </form>
      </main>
 
 <?php 
